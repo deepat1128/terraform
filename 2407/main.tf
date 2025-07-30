74b6b06e687ad72067eef457311e0200c0e2197d
deepat1128 <deepaoum1128@gmail.com>
2025-07-30 10:54:29 +0530

    Update main.tf

--- 2407/main.tf
+++ 2407/main.tf
@@ -1,6 +1,6 @@
 resource "local_file" "f1" {
           filename = var.filename1[count.index]
-          content = "test"
+          content = "count looping"
           count = lenghth(var.filename)
           }
 variable filename {
@@ -9,10 +9,25 @@ variable filename {
           }
 resource "local_file" "f2" {
           filename = each.value
-          for_each = to set for [var.filename2]
-          content = " 123 "
+          for_each = toset[var.filename2]
+          content = " for each looping "
           }
 variable "filename2" {
         type = list(string)
         default = ["aa","aa","cc","cc","bb"]
         }
+resource "local_file" "f3" {
+          filename = var.filename3
+          content = "implicit or direct dependency"
+          }
+variable filename3 {
+          type = string
+          default = " deepa "
+          }
+resource "local_file" "f4" {
+          filename = local_file.f3.id
+          content = " implicit dependency "
+          }
+
+          
+          
