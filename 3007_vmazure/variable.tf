c6280b858fc191b66f39852b5cbd935652fe2591
deepat1128 <deepaoum1128@gmail.com>
2025-08-01 16:22:01 +0530

    Add files via upload

---
+++ 3007_vmazure/variable.tf
@@ -0,0 +1,13 @@
+variable rgname {
+	default = "rg-deepa"
+	}
+variable location {
+	default = "West US 2"
+	}
+variable vnetname {
+	default = "vnet-deepa"
+	}
+variable addresspace {
+	default = ["10.0.0.0/16"]
+	type = list(string)
+	}
