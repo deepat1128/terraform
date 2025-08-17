8afec7394a5c58d59fca978da801ed8a12d6d1b1
deepat1128 <deepaoum1128@gmail.com>
2025-08-17 20:25:43 +0530

    Update provider.tf

--- 3007_vmazure/provider.tf
+++ 3007_vmazure/provider.tf
@@ -11,9 +11,8 @@ provider "azurerm" {
 
 features {}
 
-subscription_id = "2cd6d428-316d-4f0e-8101-006affa7efc1"
-client_id = "194037ca-ce17-4df1-a380-23fd62e04f21"
-client_secret = "3x48Q~j9Njcrg0W4eYjWO0K~x~_mPCP1XN5wKaTc"
-tenant_id = "8b04c66e-b2d3-49e0-892b-c799aa916ea9"
-
+subscription_id = 
+client_id =
+client_secret = 
+tenant_id =
 }
