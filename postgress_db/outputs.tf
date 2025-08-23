a2d1bdf11ebebed70cc6248aa8f3242b1d041c13
deepat1128 <deepaoum1128@gmail.com>
2025-08-24 01:12:28 +0530

    Update outputs.tf

--- postgress_db/outputs.tf
+++ postgress_db/outputs.tf
@@ -1,12 +1,7 @@
 output "postgres_connection_string" {
-  value = format(
-    "Host=%s;Database=%s;Username=%s;Password=%s;Port=5432;",
-    azurerm_postgresql_flexible_server.postgres_server.fqdn,
-    azurerm_postgresql_flexible_server_database.db.name,
-    azurerm_postgresql_flexible_server.postgres_server.administrator_login,
-    azurerm_postgresql_flexible_server.postgres_server.administrator_password
-  )
+  value = "Host=${azurerm_postgresql_flexible_server.postgres_server.fqdn};Database=${azurerm_postgresql_flexible_server_database.db.name};Username=${azurerm_postgresql_flexible_server.postgres_server.administrator_login};Password=${azurerm_postgresql_flexible_server.postgres_server.administrator_password};Port=5432;"
 }
+
 
 
 
