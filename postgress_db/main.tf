6b60dc16ccc24fd6a5a917d7e877876a173ab174
deepat1128 <deepaoum1128@gmail.com>
2025-08-23 09:44:43 -0700

    add postgress_db/main.tf

---
+++ postgress_db/main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "deepa-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_postgresql_flexible_server" "postgres_server" {
+  name                   = "deepa-postgres"
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  administrator_login    = "psqladmin"
+  administrator_password = var.admin_password
+  storage_mb             = 32768
+  sku_name               = "Standard_B1ms"
+  version                = "13"
+}
+
+resource "azurerm_postgresql_flexible_server_database" "db" {
+  name      = "deepadb"
+  server_id = azurerm_postgresql_flexible_server.postgres_server.id
+  collation = "en_US.utf8"
+  charset   = "UTF8"
+}
+
