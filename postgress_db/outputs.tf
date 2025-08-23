e9c39ab28cbcd9c94e67f5adf51b88d388b8b0c2
deepat1128 <deepaoum1128@gmail.com>
2025-08-23 09:47:34 -0700

    add postgress_db/outputs.tf

---
+++ postgress_db/outputs.tf
@@ -0,0 +1,8 @@
+output "postgres_connection_string" {
+  value = "Host=${azurerm_postgresql_flexible_server.postgres_server.fqdn};
+Database=${azurerm_postgresql_flexible_server_database.db.name};
+Username=${azurerm_postgresql_flexible_server.postgres_server.administrator_login};
+Password=${azurerm_postgresql_flexible_server.postgres_server.administrator_password};
+Port=5432;"
+}
+
