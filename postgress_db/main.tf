0e597dbb9772a595714cdcde5fe2e373d7bc1178
deepat1128 <deepaoum1128@gmail.com>
2025-08-24 00:49:07 +0530

    Update main.tf

--- postgress_db/main.tf
+++ postgress_db/main.tf
@@ -10,13 +10,15 @@ resource "azurerm_postgresql_flexible_server" "postgres_server" {
   administrator_login    = "psqladmin"
   administrator_password = var.admin_password
   storage_mb             = 32768
-  sku_name               = "Standard_B1ms"
+ sku_name = "GP_Standard_D2s_v3"
   version                = "13"
 }
+
 resource "azurerm_postgresql_flexible_server_database" "db" {
   name      = "deepadb"
   server_id = azurerm_postgresql_flexible_server.postgres_server.id
   collation = "en_US.utf8"
   charset   = "UTF8"
 }
+
 
